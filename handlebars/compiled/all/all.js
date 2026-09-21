2df825db18382428edd1b93eff4561584e7150ab
Charles Carroll <zarlss43@gmail.com>
2026-09-21 18:41:09 -0400

    handlebars cleanup

--- handlebars/compiled/all/all.js
+++ handlebars/compiled/all/all.js
@@ -26,7 +26,7 @@ this["custom"]["templates"]["faicons"] = Handlebars.template({"1":function(conta
 },"useData":true});
 
 this["custom"]["templates"]["template_secondary"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.escapeExpression, alias3=container.lambda, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3=container.escapeExpression, alias4=container.lambda, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
           return parent[propertyName];
         }
@@ -34,31 +34,16 @@ this["custom"]["templates"]["template_secondary"] = Handlebars.template({"1":fun
     };
 
   return "            <li class=\"rounded-sm mg-b-sm bg-c-gray-light pd-sm\">\n                <h4>Index: "
-    + alias2(((helper = (helper = lookupProperty(helpers,"index") || (data && lookupProperty(data,"index"))) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"index","hash":{},"data":data,"loc":{"start":{"line":5,"column":27},"end":{"line":5,"column":37}}}) : helper)))
+    + alias3(((helper = (helper = lookupProperty(helpers,"index") || (data && lookupProperty(data,"index"))) != null ? helper : alias2),(typeof helper === "function" ? helper.call(alias1,{"name":"index","hash":{},"data":data,"loc":{"start":{"line":5,"column":27},"end":{"line":5,"column":37}}}) : helper)))
     + "</h4>\n                <p>Hi, my name is <strong>"
-    + alias2(alias3(((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"name") : stack1), depth0))
-    + "</strong> I am a "
-    + alias2(alias3(((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"occupation") : stack1), depth0))
+    + alias3(alias4(((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"name") : stack1), depth0))
+    + "</strong> and I am "
+    + alias3((lookupProperty(helpers,"article")||(depth0 && lookupProperty(depth0,"article"))||alias2).call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"occupation") : stack1),{"name":"article","hash":{},"data":data,"loc":{"start":{"line":6,"column":80},"end":{"line":6,"column":113}}}))
+    + " "
+    + alias3(alias4(((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"occupation") : stack1), depth0))
     + ". I know "
-    + ((stack1 = lookupProperty(helpers,"each").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"skills") : stack1),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":6,"column":112},"end":{"line":6,"column":223}}})) != null ? stack1 : "")
+    + alias3((lookupProperty(helpers,"listJoin")||(depth0 && lookupProperty(depth0,"listJoin"))||alias2).call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"skills") : stack1),{"name":"listJoin","hash":{},"data":data,"loc":{"start":{"line":6,"column":148},"end":{"line":6,"column":178}}}))
     + ".</p>\n            </li>\n";
-},"2":function(container,depth0,helpers,partials,data) {
-    var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {}), lookupProperty = container.lookupProperty || function(parent, propertyName) {
-        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
-          return parent[propertyName];
-        }
-        return undefined
-    };
-
-  return "<span>"
-    + ((stack1 = lookupProperty(helpers,"if").call(alias1,(data && lookupProperty(data,"last")),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":6,"column":145},"end":{"line":6,"column":169}}})) != null ? stack1 : "")
-    + container.escapeExpression(container.lambda(depth0, depth0))
-    + ((stack1 = lookupProperty(helpers,"unless").call(alias1,(data && lookupProperty(data,"last")),{"name":"unless","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":6,"column":177},"end":{"line":6,"column":207}}})) != null ? stack1 : "")
-    + "</span>";
-},"3":function(container,depth0,helpers,partials,data) {
-    return "and ";
-},"5":function(container,depth0,helpers,partials,data) {
-    return ", ";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -86,7 +71,7 @@ this["custom"]["templates"]["template"] = Handlebars.template({"1":function(cont
     + alias2(alias3(((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"name") : stack1), depth0))
     + "<br>\n                    <strong>Occupation: </strong>"
     + alias2(alias3(((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"occupation") : stack1), depth0))
-    + ".<br>\n                    <strong>Skills:</strong>\n                </p>\n                <ul>\n"
+    + "<br>\n                    <strong>Skills:</strong>\n                </p>\n                <ul>\n"
     + ((stack1 = lookupProperty(helpers,"each").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"skills") : stack1),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":12,"column":20},"end":{"line":14,"column":29}}})) != null ? stack1 : "")
     + "                </ul>\n            </li>\n";
 },"2":function(container,depth0,helpers,partials,data) {
--- handlebars/compiled/template.js
+++ handlebars/compiled/template.js
@@ -15,7 +15,7 @@ this["custom"]["templates"]["template"] = Handlebars.template({"1":function(cont
     + alias2(alias3(((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"name") : stack1), depth0))
     + "<br>\n                    <strong>Occupation: </strong>"
     + alias2(alias3(((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"occupation") : stack1), depth0))
-    + ".<br>\n                    <strong>Skills:</strong>\n                </p>\n                <ul>\n"
+    + "<br>\n                    <strong>Skills:</strong>\n                </p>\n                <ul>\n"
     + ((stack1 = lookupProperty(helpers,"each").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"properties") : depth0)) != null ? lookupProperty(stack1,"skills") : stack1),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":12,"column":20},"end":{"line":14,"column":29}}})) != null ? stack1 : "")
     + "                </ul>\n            </li>\n";
 },"2":function(container,depth0,helpers,partials,data) {
